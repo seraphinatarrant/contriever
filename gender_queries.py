cbf84a4ca32c41430b532653c56db030acfd7950
Seraphina Goldfarb-Tarrant <seraphina@meta.com>
2022-11-17 11:08:53 -0800

    support for fid style dataset filtering

--- gender_queries.py
+++ gender_queries.py
@@ -61,44 +61,22 @@ female_bolukbasi = list(set(all_bolukbasi) - set(male_bolukbasi))
 female_pronouns = ["she", "her", "herself"] 
 male_pronouns = ["he", "his", "him", "himself"]
 
-def setup_argparse():
-    p = argparse.ArgumentParser()
-    p.add_argument('--dataset', help='dataset to filter')
-    p.add_argument('--format', choices=['beir', 'tqa'], default='beir')
-    p.add_argument('--split', default='test')
-    return p.parse_args()
+male_terms = set(male_bolukbasi) | set(male_extra)
+female_terms = set(female_bolukbasi) | set(female_extra)
+entity_words = set(["who", "whos", "whom", "person", "whose", "name"])
 
 
-if __name__ == "__main__":
-    args = setup_argparse()
+def get_gendered_queries(queries, query_key="text"): 
+    # returns all of these, 2 dicts and 4 lists
     male_word2query, female_word2query = defaultdict(list), defaultdict(list)
     gender, complement, male, female = [], [], [], [] 
-    male_terms = set(male_bolukbasi) | set(male_extra)
-    female_terms = set(female_bolukbasi) | set(female_extra)
-    entity_words = set(["who", "whom", "person", "whose", "name"])
-
-    target = args.dataset
-    
-    # read in
-    data, queries = [], []
-
-    # with open(f"beir/{target}/corpus.jsonl") as fin:
-    #     for line in fin:
-    #         data.append(json.loads(line))
-    with open(f"beir/{target}/queries.jsonl") as fin:
-        for line in fin:
-            queries.append(json.loads(line))
-    with open(f"beir/{target}/qrels/{args.split}.tsv") as fin:
-        qrels = pd.read_csv(fin, sep='\t')
-    
-    # filter
     for q in queries:
-        words = set(q["text"].split())
+        words = set(q[query_key].lower().split())
         male_words = words & male_terms
         female_words = words & female_terms
         if words & entity_words:
             if not male_words and not female_words:
-                    complement.append(q)
+                complement.append(q)
             else:
                 gender.append(q)
                 if male_words and female_words: # both genders so don't add to subset
@@ -109,7 +87,85 @@ if __name__ == "__main__":
                 elif female_words:
                     female_word2query[female_words.pop()].append(q)
                     female.append(q)
+    return male_word2query, female_word2query, gender, complement, male, female
     
+
+
+def setup_argparse():
+    p = argparse.ArgumentParser()
+    p.add_argument('--dataset', help='dataset to filter')
+    p.add_argument('--format', choices=['beir', 'fid'], default='beir')
+    p.add_argument('--split', default='test')
+    return p.parse_args()
+
+
+if __name__ == "__main__":
+    args = setup_argparse()
+
+    target = args.dataset
+    
+    if args.format == "beir":
+        # read in
+        data, queries = [], []
+
+        # with open(f"beir/{target}/corpus.jsonl") as fin:
+        #     for line in fin:
+        #         data.append(json.loads(line))
+        with open(f"beir/{target}/queries.jsonl") as fin:
+            for line in fin:
+                queries.append(json.loads(line))
+        with open(f"beir/{target}/qrels/{args.split}.tsv") as fin:
+            qrels = pd.read_csv(fin, sep='\t')
+        
+        # filter
+        male_word2query, female_word2query, gender, complement, male, female = get_gendered_queries(queries, query_key="text")
+        
+        # save
+        base = "beir/{}-{}/"
+        gender_df = pd.DataFrame(data=gender, columns=['_id','text','metadata'])
+        male_df = pd.DataFrame(data=male, columns=['_id','text','metadata'])
+        female_df = pd.DataFrame(data=female, columns=['_id','text','metadata'])
+        complement_df = pd.DataFrame(data=complement, columns=['_id','text','metadata'])
+        print("Example gender queries:")
+        print(gender_df.sample(10)["text"].values)
+        print("Example complement queries:")
+        print(complement_df.sample(10)["text"].values)
+
+        for target_df, name in [(gender_df, "gender"), (complement_df, "complement"), (male_df, "male"), (female_df, "female")]:
+            this_base = base.format(target,name)
+            os.makedirs(this_base +"qrels/", exist_ok=True)
+            qrels_outpath, queries_outpath = this_base +"qrels/test.tsv", this_base + "queries.jsonl"
+            target_qrels = qrels[ qrels["query-id"].isin(target_df['_id']) ]
+
+            target_qrels.to_csv(qrels_outpath, sep="\t", index=None)
+            target_df.to_json(queries_outpath, lines=True, orient="records")
+            # copy train corpus to this base
+            shutil.copy2(f"beir/{target}/corpus.jsonl", this_base)
+    
+    elif args.format == "fid": #  fid datasets are nq and trivia qa (NQ) and (TQA)
+        # fields are question answers target ctxs
+        with open(f"fid_data/{target}/{args.split}.json", "r") as fin:
+            queries = json.load(fin)
+
+        # filter -- this version has everything along with the query but it's simplest to just use the same format as beir where queries are separate. Just only save one file at the end.
+        male_word2query, female_word2query, gender, complement, male, female = get_gendered_queries(queries, query_key="question")
+    
+        # save
+        base = f"fid_data/{target}/{args.split}-" + "{}.json"
+        cols = ['question', 'answers', 'target', 'ctxs']
+        gender_df = pd.DataFrame(data=gender, columns=cols)
+        male_df = pd.DataFrame(data=male, columns=cols)
+        female_df = pd.DataFrame(data=female, columns=cols)
+        complement_df = pd.DataFrame(data=complement, columns=cols)
+        print("Example gender queries:")
+        print(gender_df.sample(10)["question"].values)
+        print("Example complement queries:")
+        print(complement_df.sample(10)["question"].values)
+
+        for target_df, name in [(gender_df, "gender"), (complement_df, "complement"), (male_df, "male"), (female_df, "female")]:
+            outpath = base.format(name)
+            target_df.to_json(outpath, orient="records")
+            
     # stats
     print(
         f"num gender: {len(gender)} ({len(gender)/len(queries)*100}%)\n \
@@ -125,25 +181,5 @@ if __name__ == "__main__":
         count_male += len(male_word2query[p])
     print(f"num gender queries with pronouns (which might be noisy): \
         {count_female + count_male} ({count_female} female {count_male} male)")
-    
-    # save
-    base = "beir/{}-{}/"
-    gender_df = pd.DataFrame(data=gender, columns=['_id','text','metadata'])
-    male_df = pd.DataFrame(data=male, columns=['_id','text','metadata'])
-    female_df = pd.DataFrame(data=female, columns=['_id','text','metadata'])
-    complement_df = pd.DataFrame(data=complement, columns=['_id','text','metadata'])
-    print("Example gender queries:")
-    print(gender_df.sample(10)["text"].values)
-    print("Example complement queries:")
-    print(complement_df.sample(10)["text"].values)
 
-    for target_df, name in [(gender_df, "gender"), (complement_df, "complement"), (male_df, "male"), (female_df, "female")]:
-        this_base = base.format(target,name)
-        os.makedirs(this_base +"qrels/", exist_ok=True)
-        qrels_outpath, queries_outpath = this_base +"qrels/test.tsv", this_base + "queries.jsonl"
-        target_qrels = qrels[ qrels["query-id"].isin(target_df['_id']) ]
 
-        target_qrels.to_csv(qrels_outpath, sep="\t", index=None)
-        target_df.to_json(queries_outpath, lines=True, orient="records")
-        # copy train corpus to this base
-        shutil.copy2(f"beir/{target}/corpus.jsonl", this_base)
